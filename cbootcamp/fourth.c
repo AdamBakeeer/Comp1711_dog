ae70cdb51bade8ec5703d56f6b70ab2e3313dbf0
adam bakeer <146668872+AdamBakeeer@users.noreply.github.com>
2023-10-10 16:14:40 +0000

    First c

---
+++ cbootcamp/fourth.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+char name [10];
+printf("insert a name: ");
+scanf("%s" , name);
+printf("Hello, %s\n " , name) ;
+return 0; 
+}
---
+++ cbootcamp/second.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+float var1 ;
+printf("insert Length: ");
+float var2 ;
+scanf("%f" , &var1);
+printf("insert width: ");
+scanf("%f" , &var2);
+printf("Area is = %f\n " ,var1 * var2 ) ;
+return 0; 
+}
